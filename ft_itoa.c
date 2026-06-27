c1c0e9f40ebfa9ec28a90ba8eb883e806c1dddd4
Jaspreet Singh <jsingh@student.42warsaw.pl>
2026-06-27 20:42:05 +0200

    Libft 100% complete

---
+++ ft_itoa.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsingh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/27 16:41:44 by jsingh            #+#    #+#             */
+/*   Updated: 2026/06/27 17:21:20 by jsingh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count_digits(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+		count++;
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+static char	*fill_nbr(char *str, int n, int len)
+{
+	if ( n == -2147483648)
+	{
+		ft_strlcpy(str,"-2147483648",12);
+		return (str);
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	str[len] = '\0';
+	while (n > 0)
+	{
+		str[--len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+
+	if (n == 0)
+		return (ft_strdup("0"));
+	len = (count_digits(n));
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	return (fill_nbr(str, n, len));
+}
--- libft.h
+++ libft.h
@@ -6,55 +6,96 @@
 /*   By: jsingh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 16:28:41 by jsingh            #+#    #+#             */
-/*   Updated: 2026/06/27 01:40:43 by jsingh           ###   ########.fr       */
+/*   Updated: 2026/06/27 18:21:05 by jsingh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-//! Includes
+//! =========================== INCLUDES ===========================
 
-#include <stddef.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-// ! Part 1 - Libc functions
+//! =========================== PART 1 - LIBC FUNCTIONS ===========================
 
-int	ft_isalpha(int ch);
-int	ft_isalnum(int ch);
-int	ft_isascii(int ch);
-int	ft_isprint(int ch);
-int	ft_tolower(int ch);
-int	ft_toupper(int ch);
-int	ft_isdigit(int ch);
+// Character checks
+int		ft_isalpha(int ch);
+int		ft_isdigit(int ch);
+int		ft_isalnum(int ch);
+int		ft_isascii(int ch);
+int		ft_isprint(int ch);
+int		ft_tolower(int ch);
+int		ft_toupper(int ch);
 
-// strings functions
-
+// String functions
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
-// memory functions 
+// String copy/concat
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
+// Memory functions
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-// convertir functions
+// Memory allocation (uses malloc)
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
-int	ft_atoi(const char *nptr);
+// Conversion functions
+int		ft_atoi(const char *nptr);
+char	*ft_itoa(int n);  // ← Part 2? Actually Part 2 in subject!
 
+//! =========================== PART 2 - ADDITIONAL FUNCTIONS ===========================
 
-// ! Part 2 - Additional functions
+// String manipulation (uses malloc)
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
+// Iteration functions
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
+// Output functions (uses write)
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
+//! =========================== PART 3 - LINKED LIST (BONUS) ===========================
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+// List creation/manipulation
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+
+// List deletion
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+
+// List iteration
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
