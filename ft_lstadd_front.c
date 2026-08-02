37299ca08471770fbf5e1da4500f62de9fe334ea
Jaspreet Singh <jsingh@student.42warsaw.pl>
2026-08-02 15:45:00 +0200

    final

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsingh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: jsingh <jsingh@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 23:58:36 by jsingh            #+#    #+#             */
-/*   Updated: 2026/07/06 11:22:52 by jsingh           ###   ########.fr       */
+/*   Updated: 2026/07/14 16:44:03 by jsingh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,3 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include "libft.h"
-
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*node;
-
-	node = malloc(sizeof(t_list));
-	if (!node)
-		return (NULL);
-	node->content = content;
-	node->next = NULL;
-	return (node);
-}
-
-int	main(void)
-{
-	t_list	*list = NULL;
-	t_list	*node1;
-	t_list	*node2;
-
-	// Create and add first node
-	node1 = ft_lstnew("Hello");
-	ft_lstadd_front(&list, node1);
-	printf("Test 1: %s\n", (char *)list->content);
-
-	// Create and add second node
-	node2 = ft_lstnew("World");
-	ft_lstadd_front(&list, node2);
-	printf("Test 2: %s → %s\n", 
-	       (char *)list->content,      // World
-	       (char *)list->next->content); // Hello
-
-	// Free all nodes (in reverse order of addition)
-	free(node2);
-	free(node1);
-
-	return (0);
-}*/
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsingh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: jsingh <jsingh@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 11:23:34 by jsingh            #+#    #+#             */
-/*   Updated: 2026/07/06 11:37:07 by jsingh           ###   ########.fr       */
+/*   Updated: 2026/07/14 18:16:08 by jsingh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,25 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (counter);
 }
+
+// #include <stdio.h>
+// int main()
+// {
+// 	t_list *list = NULL;
+// 	t_list *first = ft_lstnew("1st");
+// 	t_list *second = ft_lstnew("2nd");
+// 	t_list *third = ft_lstnew("3rd");
+
+// 	ft_lstadd_back(&list , first);
+// 	ft_lstadd_back(&list , second);
+// 	ft_lstadd_back(&list , third);
+
+// 	int size = ft_lstsize(list);
+// 	printf("%d", size);
+
+// 	free(first);
+// 	free(second);
+// 	free(third);
+// 	return (0);
+
+// }
